e9d6eb26f38de1c2e086b1daac4031babd6934ae
Daphne Calin <daphnecalin@gmail.com>
2023-10-26 18:17:46 -0400

    implement decoder function

--- main.py
+++ main.py
@@ -6,4 +6,10 @@ def encoder(password):
         pw_return += str(value)
     return pw_return
 
-
+def decoder(password):
+    pw_return = ""
+    value = 0
+    for i in range(len(password)):
+        value = int(password[i]) - 3
+        pw_return += str(value)
+    return pw_return
